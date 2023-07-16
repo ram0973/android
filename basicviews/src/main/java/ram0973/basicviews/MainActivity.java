415e6db4a83bb7c7911fcb8278b07c3ae5d57a15
ram0973 <ram0973@gmail.com>
2023-07-16 15:02:41 +0300

    initial commit

---
+++ basicviews/src/main/java/ram0973/basicviews/MainActivity.java
@@ -0,0 +1,13 @@
+package ram0973.basicviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ layoutfiles/src/main/java/ram0973/layoutfiles/MainActivity.java
@@ -0,0 +1,13 @@
+package ram0973.layoutfiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscreen);
+    }
+}
